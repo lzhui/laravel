a1af1f2e97853eef77dfeb08ed73358107ed27bd
lzhui <921424330@qq.com>
2016-10-17 00:50:03 +0800

    init

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+
+class TestController extends Controller
+{
+    public function index(){
+        echo "<br>Test Controller.";
+    }
+}
